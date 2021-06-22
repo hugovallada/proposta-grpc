e874ac45d43aa98608b914c9c1e4bbb67220dec4
hugovallada <valladahugo@gmail.com>
2021-06-22 14:45:41 -0300

    Add table_relationship migration

---
+++ src/main/resources/db/migration/V17__create_relation_table_credit_card_wallet.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tb_credit_card_associate_wallet(
+    id serial not null primary key,
+    credit_card_id int not null,
+    wallet_id int not null,
+    foreign key(credit_card_id) references tb_credit_card(id),
+    foreign key(wallet_id) references tb_wallet(id)
+);
